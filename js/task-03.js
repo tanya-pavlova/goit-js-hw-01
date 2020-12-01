47bbdd3a9546454469a3bc564f2dcaa932c9a66c
tanya-pavlova <70815370+tanya-pavlova@users.noreply.github.com>
2020-12-01 22:03:30 +0300

    исправлена отмена

--- js/task-03.js
+++ js/task-03.js
@@ -1,12 +1,19 @@
- 
-const answer = prompt('Введите пароль') || 'Отменено пользователем';
-
-console.log(answer);
 
 const ADMIN_PASSWORD ='jqueryismyjam';
 
-let message = answer === ADMIN_PASSWORD 
-    ? 'Добро пожаловать!'
-    : 'Доступ запрещен, неверный пароль!';
+let message;
 
-alert(message);
+const answer = prompt('Введите пароль');
+
+if (answer === null) {
+message = 'Отменено пользователем';
+alert(message)
+}
+else if (answer === ADMIN_PASSWORD) {
+    message = 'Добро пожаловать!';
+    alert(message)   
+}
+else {
+    message = 'Доступ запрещен, неверный пароль!';
+    alert(message);
+}
